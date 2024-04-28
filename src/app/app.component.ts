acbfa80cc52ed254008724817b2d69af88bf8888
Alexandre <vensalex1991@gmail.com>
2024-04-28 10:39:31 +0200

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+//-------------------------------------------------------------------------
+// Angular Core
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// PrimeNG
 import { RouterOutlet } from '@angular/router';
-
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { BannerComponent } from './banner/banner.component';
+//-------------------------------------------------------------------------
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuModule, CommonModule, BannerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cheatset-maelle';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Pages',
+        items: [
+          { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: '/' },
+          {
+            label: 'Loops',
+            icon: 'pi pi-sync',
+            routerLink: '/loops',
+          },
+        ],
+      },
+    ];
+  }
 }
